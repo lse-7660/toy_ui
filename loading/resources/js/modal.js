0967f58fd855d02f31a8e013bf6cc8e57a29598c
lse-7660 <lse76609251@gmail.com>
2025-04-17 17:05:12 +0900

    modal.js html 변수명 변경

--- loading/resources/js/modal.js
+++ loading/resources/js/modal.js
@@ -1,3 +1,5 @@
+const htmlM = document.querySelector('html');
+
 const modal = document.getElementById('modal');
 const modalImages = document.getElementById('modal-images');
 const closeBtn = document.querySelector('.close');
@@ -5,7 +7,7 @@ const closeBtn = document.querySelector('.close');
 // image-box들 클릭 이벤트
 document.querySelectorAll('.image-box').forEach((box) => {
     box.addEventListener('click', () => {
-        html.style.overflow = 'hidden';
+        htmlM.style.overflow = 'hidden';
         // 이미지 데이터: dataset 속성으로 가져오기
         const imgs = box.dataset.images.split(',');
 
@@ -26,13 +28,13 @@ document.querySelectorAll('.image-box').forEach((box) => {
 // 닫기 버튼 이벤트
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
-    html.style.overflow = 'auto'; // 스크롤 해제
+    htmlM.style.overflow = 'auto'; // 스크롤 해제
 });
 
 // 모달 바깥 클릭 시 닫기
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
-        html.style.overflow = 'auto';
+        htmlM.style.overflow = 'auto';
     }
 });
